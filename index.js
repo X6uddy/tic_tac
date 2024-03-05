3ab8318ad204575323dae516918cb5cba4d7d480
Volkov Sasha <sashavolkov_03@mail.ru>
2024-03-05 20:23:28 +0400

    added logic for connection two players

--- index.js
+++ index.js
@@ -7,6 +7,39 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+let waitArray = [];
+let playingArray = [];
+
+io.on('connection', (socket) => {
+    socket.on('find', (e) => {
+        if(e.name != null || e.name != '') {
+            waitArray.push(e.name);
+
+            if(waitArray.length >= 2) {
+                console.log(waitArray);
+                let p1obj = {
+                    p1name: waitArray[0],
+                    p1value: 'X',
+                    p1move: ''
+                }
+                let p2obj = {
+                    p2name: waitArray[1],
+                    p2value: 'O',
+                    p2move: ''
+                }
+                let mainObj = {
+                    p1: p1obj,
+                    p2: p2obj
+                }
+                playingArray.push(mainObj);
+                waitArray.splice(0, 2);
+
+                io.emit('find', {allPlayers: playingArray});
+            }
+        }
+    })
+})
+
 app.use(express.static(path.resolve('')))
 app.get('/', (req, res) => {
     res.sendFile('index.html');
